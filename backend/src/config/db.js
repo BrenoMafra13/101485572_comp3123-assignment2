8a1557d89feb57c08814f22a779ecf7fd99e4087
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-11-10 22:35:43 -0500

    Finishing backend configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,9 +5,9 @@ export const connectDB = async (uri) => {
     await mongoose.connect(uri, {
       dbName: "comp3123_db",
     });
-    console.log("✅ MongoDB connected");
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
+    console.error("MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
